40e0b42f8d16cc3151a3e35d460534bba19feab0
3idey <elaidya225@gmail.com>
2025-07-23 21:25:56 +0300

    update the logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,18 +4,18 @@
         <nav
             class="w-64 bg-gray-900 text-white flex flex-col items-center py-8 space-y-6 fixed left-0 top-0 h-full shadow-lg">
             <!-- Logo -->
-            <a href="/" class="flex items-center space-x-3">
-                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/logo.png') }}" alt="logo"
-                    class="w-14 h-14 object-contain rounded-full hover:scale-105 transition-transform duration-300 ease-in-out">
-                <span class="text-lg font-bold">.poster</span>
+            <a href="/" class="flex items-center space-x-3 px-4">
+                <img src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/fulllogo.png') }}" alt="logo"
+                    class="w-40 h-40 object-cover  hover:scale-115 transition-transform duration-300 ease-in-out">
             </a>
 
             <!-- Navigation Links -->
-            <ul class="w-full px-6 space-y-4 mt-10">
-                <li><x-link link="/">Home</x-link></li>
-                <li><x-link link="#">Posters</x-link></li>
-                <li><x-link link="#">Contact</x-link></li>
-                <li><x-link link="/about">About</x-link></li>
+            <ul class="w-full px-6 space-y-4 mt-1">
+                <li><x-sidelink link="/">Home</x-sidelink></li>
+                <li><x-sidelink link="#">Posters</x-sidelink></li>
+                <li><x-sidelink link="#">Contact</x-sidelink></li>
+                <li><x-sidelink link="/about">About</x-sidelink></li>
+
             </ul>
         </nav>
 
